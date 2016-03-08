d18150444e2381cd510f2f5a7e0c5e9a429c99eb
ZhHong <371816216@qq.com>
2016-03-08 23:29:59 +0800

    add input data

--- moneyrecord/Assets/app/actions/CreateRecordAction.cs
+++ moneyrecord/Assets/app/actions/CreateRecordAction.cs
@@ -1,5 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+using System.Text.RegularExpressions;
+using UnityEngine.UI;
 using app.manager;
 
 public class CreateRecordAction : MonoBehaviour {
@@ -37,42 +40,72 @@ public class CreateRecordAction : MonoBehaviour {
     }
 
 
-    public void OnMoneyInputEnd(string money)
+    public void setMoneyInputEnd(string payStr)
     {
         //on money input end check it
+        double temp = double.Parse(payStr);
+        payValue=Math.Round(temp, 2);
     }
 
-    public void OnMsgInputEnd(string msg)
+    public void setMsgInputEnd(string msg)
     {
         //on msg input end check it
+        Debug.Log("================" + msg.Length);
+        
     }
 
 
-    public void OnYearSelected(int index) {
+    public void setYearSelected(Dropdown drp) {
         //on year selected
+        year = int.Parse(drp.captionText.text);
     }
 
-    public void OnMonthSelected(int index)
+    public void setMonthSelected(Dropdown drp)
     {
-        //on year selected
+        month = int.Parse(drp.captionText.text);
     }
 
-    public void OnDaySelected(int index) {
-
+    public void setDaySelected(Dropdown drp) {
+        //need check month 2
+        day = int.Parse(drp.captionText.text);
     }
 
-    public void OnMoneyClassSelected(int index)
+    public void setMoneyClassSelected(Dropdown drp)
     {
-
+        moneyClass = int.Parse(drp.captionText.text);
     }
 
-    public void OnPayTypeSelected(int index)
+    public void setPayTypeSelected(Dropdown drp)
     {
-
+        payType = int.Parse(drp.captionText.text);
     }
 
     private void SetTextComponetTextState(string componetName,bool state) {
         //set warning text
+    }
+
+
+    private void CheckMonth2Day() {
+        //check month 2
+        bool changeDay = false;
+        if (year !=0 && month !=0 && day!=0 && ((year%100 !=0 && year%4 ==0)|(year %400 ==0)) && month ==2) {
+            if(day > 29)
+            {
+                day = 29;
+                changeDay = true;
+            }
+        }
+        else
+        {
+            if (day >28) {
+                day = 28;
+                changeDay = true;
+            }
+        }
+        if (changeDay)
+        {
+            SetTextComponetTextState("TextTimeWarning",false);
+        }
     }
 
     private void ResetData() {
