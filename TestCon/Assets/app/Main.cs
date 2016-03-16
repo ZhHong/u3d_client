6eae1e629909d8943fd15bd19f56a83562c39df8
ZhHong <371816216@qq.com>
2016-03-16 17:52:13 +0800

    Test network with singularity

---
+++ TestCon/Assets/app/Main.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+
+	private string show_str = "";
+	private LFL_NetWork network = null;
+
+	private string modelId = "";
+	private string actionId ="";
+
+	private string msg = "";
+
+	// Use this for initialization
+	void Start () {
+		if (network == null){
+			network = LFL_NetWork.getInstance ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+		GUI.TextArea (new Rect (10, 10, 500, 400), show_str);
+		modelId =GUI.TextField (new Rect (520, 10, 80, 30), modelId);
+		actionId=GUI.TextField (new Rect (600, 10, 80, 30), actionId);
+		msg =GUI.TextField (new Rect (520, 50, 160, 30), msg);
+		if (GUI.Button (new Rect (690, 10, 160, 30), "ADD_STR")) {
+			show_str += "aaaaaaaaa\n";
+		}
+		if (GUI.Button (new Rect (690, 50, 160, 30), "SEND")) {
+			string str = "SEND_MSG::" + modelId + "-" + actionId + " data =" + msg;
+
+			show_str += str+ "\n";
+
+		}
+		if (GUI.Button (new Rect(690,90,160,30),"connect")){
+			network.connectServer ();
+		}
+		if (GUI.Button(new Rect(690,130,160,30),"disconnect")){
+			network.closeConnection ();
+		}
+
+	}
+}
---
+++ TestCon/Assets/app/Utils.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Utils{
+
+	public static int GetNowTime() {
+		System.DateTime starttime = System.TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1, 0, 0, 0, 0));
+		System.DateTime nowTime = System.DateTime.Now;
+		int unixtime = (int)System.Math.Round((nowTime-starttime).TotalSeconds,System.MidpointRounding.AwayFromZero);
+
+		return unixtime;
+	}
+
+	public static string GetNowTimeStr(){
+		return "";
+	}
+
+	public static int GetStrTime(int year,int month,int day){
+		return 0;
+	}
+
+	public static DateTime GetTimeStr (int time){
+		DateTime dt = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970,1,1));
+		long itime = long.Parse(time+"0000000");
+		TimeSpan tt = new TimeSpan(itime);
+		return dt.Add(tt);
+	}
+}
