867a071c6931433892200fab4671801655b14f4e
ZhHong <371816216@qq.com>
2016-03-17 20:20:47 +0800

    add table view test

--- moneyrecord/Assets/app/layer/TableLayer.cs
+++ moneyrecord/Assets/app/layer/TableLayer.cs
@@ -3,13 +3,35 @@ using System.Collections;
 
 public class TableLayer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	private static bool showTable = false;
+
+	void OnGUI(){
+		//Todo
+		//use data colums rows
+		//use data length max to decided the label lenth
+		//
+		if (!showTable){
+			return;
+		}
+		int rows = 10;
+		int colums = 10;
+		int labelWidth = 100;
+		int labeHegith = 30;
+		GUI.backgroundColor = Color.red;
+		GUI.BeginScrollView (new Rect (100, 100, labelWidth * 10, labeHegith * 10),Vector2.zero,new Rect(0,0,labelWidth*3,labeHegith*2));
+		for(int i =0;i<rows;i++){
+			for(int j=0;j<colums;j++){
+				GUI.Label (new Rect (100+(labelWidth*j), 100+(labeHegith*i), labelWidth, labeHegith), "mylabel");
+			}
+		}
+		GUI.EndScrollView ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public static void CreateTableView(){
+		
+	}
+
+	public static void Show(){
+		
 	}
 }
